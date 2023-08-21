624b8da558a49745eda868e1c7df2ea18834c4ce
onurbyr <byronurr@gmail.com>
2023-08-21 22:54:06 +0300

    Home 2

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,19 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View, ToastAndroid} from 'react-native';
 import Mapbox from '@rnmapbox/maps';
-import Geolocation from '@react-native-community/geolocation';
 import RNAndroidLocationEnabler from 'react-native-android-location-enabler';
 import {Button} from 'react-native-paper';
+import {isLocationEnabled, hasGms} from 'react-native-device-info';
+import GeoJSON from 'geojson';
 
 Mapbox.setAccessToken(
   'pk.eyJ1IjoibW90bzEyIiwiYSI6ImNsbGpleW1razF1cnMzZ21naWlyYnVnbWMifQ.y8aUKafZWu9sY9o8aiVXBw',
 );
 
 const Home = () => {
-  const [currentCoords, setCurrentCoords] = useState([0, 0]);
   const [isTrackingEnabled, setIsTrackingEnabled] = useState(true);
+  const [isPermissionsOK, setIsPermissionsOK] = useState(false);
+  const [savedLocations, setSavedLocations] = useState([]);
 
   useEffect(() => {
     checkLocationPermission();
@@ -27,34 +29,41 @@ const Home = () => {
       );
       return;
     }
-    RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({
-      interval: 10000,
-      fastInterval: 5000,
-    })
-      .then(data => {
-        // getCurrentLocation();
-      })
-      .catch(err => {
-        if (err.code === 'ERR00') {
-          ToastAndroid.show(
-            'You have to enable gps for using the app',
-            ToastAndroid.SHORT,
-          );
-        }
-      });
-  };
 
-  const getCurrentLocation = () => {
-    Geolocation.getCurrentPosition(
-      location =>
-        setCurrentCoords([location.coords.longitude, location.coords.latitude]),
-      error =>
-        ToastAndroid.show(
-          `GetCurrentPosition Error ${JSON.stringify(error)}`,
-          ToastAndroid.SHORT,
-        ),
-      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-    );
+    //Telefonda googlde servisleri kullanılabilir mi kontrol
+    hasGms().then(status => {
+      //Kullanabilir ise
+      if (status) {
+        RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({
+          interval: 10000,
+          fastInterval: 5000,
+        })
+          .then(data => {
+            setIsPermissionsOK(true);
+          })
+          .catch(err => {
+            if (err.code === 'ERR00') {
+              ToastAndroid.show(
+                'You have to enable gps for using the app',
+                ToastAndroid.LONG,
+              );
+            }
+          });
+      } else {
+        //Değil ise
+        //Gps kontrol et
+        isLocationEnabled().then(enabled => {
+          if (enabled) {
+            setIsPermissionsOK(true);
+          } else {
+            ToastAndroid.show(
+              'You have to enable gps for using the app',
+              ToastAndroid.LONG,
+            );
+          }
+        });
+      }
+    });
   };
 
   return (
@@ -64,24 +73,47 @@ const Home = () => {
         zoomEnabled={true}
         rotateEnabled={true}>
         <Mapbox.Camera
-          zoomLevel={15}
-          centerCoordinate={currentCoords}
-          pitch={10}
+          followUserLocation={isTrackingEnabled}
+          followZoomLevel={16}
           animationMode="flyTo"
           animationDuration={3000}
         />
-        <Mapbox.UserLocation
-          visible={true}
-          androidRenderMode="normal"
-          // minDisplacement={5}
-          onUpdate={location => {
-            console.log(location);
-            setCurrentCoords([
-              location.coords.longitude,
-              location.coords.latitude,
-            ]);
-          }}
-        />
+        {isPermissionsOK ? (
+          <Mapbox.UserLocation
+            androidRenderMode="normal"
+            minDisplacement={5}
+            onUpdate={location => {
+              setSavedLocations(current => [...current, location.coords]);
+            }}
+          />
+        ) : null}
+        <Mapbox.ShapeSource
+          id="locations"
+          shape={GeoJSON.parse(savedLocations, {
+            Point: ['latitude', 'longitude'],
+          })}>
+          <Mapbox.HeatmapLayer
+            id="locations"
+            sourceID="locations"
+            style={{
+              heatmapColor: [
+                'interpolate',
+                ['linear'],
+                ['heatmap-density'],
+                0,
+                'rgba(236,222,239,0)',
+                0.2,
+                'rgb(208,209,230)',
+                0.4,
+                'rgb(166,189,219)',
+                0.6,
+                'rgb(103,169,207)',
+                0.8,
+                'rgb(28,144,153)',
+              ],
+            }}
+          />
+        </Mapbox.ShapeSource>
       </Mapbox.MapView>
       <Button
         onPress={() => {
@@ -92,7 +124,6 @@ const Home = () => {
         }}>
         {isTrackingEnabled ? 'Disable' : 'Enable'} Tracking
       </Button>
-      <Button onPress={() => getCurrentLocation()}>Update Location</Button>
     </View>
   );
 };
